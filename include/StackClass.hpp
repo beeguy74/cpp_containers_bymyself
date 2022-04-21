a78aac357b2a6bbe2801808125d63e69065476d3
Neverov Yaroslav <beeguy74@gmail.com>
2022-04-21 15:31:04 +0300

    changes in stack like in c++ stl book

--- include/StackClass.hpp
+++ include/StackClass.hpp
@@ -4,10 +4,22 @@
 # include <vector>
 
 template <class T, class Container = std::vector<T> >
-	class Stack {
-    public:
-        stack() {};
-        ~stack(void) {};
-	};
+class stack {
+protected:
+    Container _cont;
+public:
+    typedef Container container_type;
+    typedef typename Container::value_type value_type;
+    typedef typename Container::size_type size_type;
+    // explicit make constructor closed to implicit conversions
+    explicit stack(const container_type& cont = container_type()) : _cont(cont) {};
+    bool empty() const { return _cont.empty(); };
+    size_type size() const { return _cont.empty(); };
+    value_type& top() { return _cont.back() };
+    const value_type& top() cosnt { return _cont.back() };
+    void push(const value_type& x) { _cont.push_back(x); };
+    void pop() { _cont.pop_back() };
+    ~stack(void) {};
+};
     
 #endif
