1183e2d150c0f39c27c956ffb8cc28b72303eb40
Neverov Yaroslav <beeguy74@gmail.com>
2022-04-23 15:34:14 +0300

    need to realize what typedef ptrit means

--- include/vector.hpp
+++ include/vector.hpp
@@ -13,14 +13,20 @@ namespace ft
         typedef size_t                      size_type;
         typedef A                           allocator_type;
         typedef typename A::ponter          Tptr;
-        typedef typename A::const_pointer   const_pointer;
+        typedef typename A::const_pointer   Ctptr;
         typedef typename A::reference       reference;
         typedef typename A::const_reference const_reference;
         typedef typename A::value_type      value_type;
         // The container may have an internal need to allocate objects of a different type, 
         // and tempalte rebind<T>::other::difference_type holds allocator of that type 
         // https://eel.is/c++draft/allocator.requirements
-        typedef typename A::template rebind<T>::other::difference_type             difference_type;
+        typedef typename A::template rebind<T>::other::difference_type
+            difference_type;
+        typedef Ptrit<value_type, difference_type, Tptr, reference, Tptr, reference>
+            iterator;
+        typedef Ptrit<value_type, difference_type, Ctptr, const_reference, Tptr, reference>
+            const_iterator;
+
 
     };
 
