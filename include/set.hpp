258ba85e5b89df3dea12343a2f6744ea68ce56b1
Iaroslav NEVEROV <beeguy74@gmail.com>
2022-11-13 20:04:13 +0100

    set is done

--- include/set.hpp
+++ include/set.hpp
@@ -30,6 +30,44 @@ namespace ft {
             class Pr = std::less<K>,
             class A = std::allocator<K> >
     class set : public ft::Tree<Tset_traits<K, Pr, A, false> > {
+    public:
+        typedef set<K, Pr, A>   Myt;
+        typedef ft::Tree<Tset_traits<K, Pr, A, false> >     Mybase;
+        typedef K   key_type;
+        typedef Pr  key_compare;
+        typedef typename Mybase::value_compare  value_compare;
+        typedef typename Mybase::allocator_type allocator_type;
+        typedef typenae Mybase::size_type       size_type;
+        typedef typename Mybase::difference_type    difference_type;
+        typedef typename Mybase::pointer        pointer;
+        typedef typename Mybase::const_pointer  const_pointer;
+        typedef typename Mybase::reference      reference;
+        typedef typename Mybase::const_reference    const_reference;
+        typedef typename Mybase::iterator       iterator;
+        typedef typename Mybase::const_iterator const_iterator;
+        typedef typename Mybase::reverse_iterator   reverse_iterator;
+        typedef typename Mybase::const_reverse_iterator const_reverse_iterator;
+        typedef typename Mybase::value_type     value_type;
+
+        set() : Mybase(key_compare(), allocator_type()) {}
+        explicit set(const key_compare& Pred) : Mybase(Pred, allocator_type()) {}
+        set(const key_compare& Pred, const allocator_type& Al) : Mybase(Pred, Al) {}
+        template<class It>
+        set(It F, It L) : Mybase(key_compare(), allocator_type()){
+            for (; F != L; ++F)
+                insert (*F);
+        }
+        template<class It>
+        set(it F, It L, const key_compare& Pred): Mybase(Pred, allocator_type()){
+            for (; F != L; ++F)
+                insert (*F);
+        }
+        template<class It>
+        set(it F, It L, const key_compare& Pred, const allocator_type& Al) 
+            : Mybase(Pred, Al){
+            for (; F != L; ++F)
+                insert (*F);
+        }
 
     }
 }
