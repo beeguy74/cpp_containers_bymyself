91bccb0e1402395f6fa88b8e55e0d4d955aa4c31
Iaroslav NEVEROV <beeguy74@gmail.com>
2022-11-24 13:13:50 +0100

    beautify map and set

--- include/map.hpp
+++ include/map.hpp
@@ -5,12 +5,6 @@
 # include "utility.hpp"
 
 namespace ft{
-    // template<class Key, class T, class Pred, class A>
-    // class map;
-
-    // template<class Key, class T, class Pred, class A>
-    // bool operator==();
-
     template<class K, class T, class Pr, class Ax, bool Mfl>
     class Tmap_traits{
     public:
@@ -43,28 +37,31 @@ namespace ft{
 
     }; //class Tmap_traits;
 
-    template<class K, class T, class Pr = std::less<K>, class A = std::allocator<pair<const K, T> > >
+    template<class K, class T,
+                class Pr = std::less<K>,
+                class A = std::allocator<pair<const K, T> > >
     class map : public Tree<Tmap_traits<K, T, Pr, A, false> >  {
     public:
         typedef map<K, T, Pr, A>        Myt;
         typedef Tree<Tmap_traits<K, T, Pr, A, false> >  Mybase;
-        typedef K           key_type;
-        typedef T           mapped_type;
-        typedef T           referent_type;//shit
-        typedef Pr          key_compare;
-        typedef typename Mybase::value_compare  value_compare;
-        typedef typename Mybase::allocator_type allocator_type;
-        typedef typename Mybase::size_type      size_type;
+        typedef K                                   key_type;
+        typedef T                                   mapped_type;
+        typedef T                                   referent_type;
+        typedef Pr                                  key_compare;
+        typedef typename Mybase::value_compare      value_compare;
+        typedef typename Mybase::allocator_type     allocator_type;
+        typedef typename Mybase::size_type          size_type;
         typedef typename Mybase::difference_type    difference_type;
-        typedef typename Mybase::pointer        pointer;
-        typedef typename Mybase::const_pointer  const_pointer;
-        typedef typename Mybase::reference      reference;
+        typedef typename Mybase::pointer            pointer;
+        typedef typename Mybase::const_pointer      const_pointer;
+        typedef typename Mybase::reference          reference;
         typedef typename Mybase::const_reference    const_reference;
-        typedef typename Mybase::iterator       iterator;
-        typedef typename Mybase::const_iterator const_iterator;
+        typedef typename Mybase::iterator           iterator;
+        typedef typename Mybase::const_iterator     const_iterator;
         typedef typename Mybase::reverse_iterator   reverse_iterator;
         typedef typename Mybase::const_reverse_iterator const_reverse_iterator;
-        typedef typename Mybase::value_type     value_type;
+        typedef typename Mybase::value_type         value_type;
+
         map() : Mybase(key_compare(), allocator_type()) {}
         explicit map(const key_compare& Pred, const allocator_type& Al) : Mybase(Pred, Al) {}
         template<class It>
@@ -88,12 +85,13 @@ namespace ft{
         }
     }; //class map;
     
-    template <class _Key, class _T, class _Compare, class _Allocator>
-        void swap(map<_Key,_T,_Compare,_Allocator>& lhs, map<_Key,_T,_Compare,_Allocator>& rhs) {lhs.swap(rhs);}
+    template <class Key, class T, class Compare, class Allocator>
+    void swap(map<Key,T,Compare,Allocator>& lhs, map<Key,T,Compare,Allocator>& rhs){
+        lhs.swap(rhs);
+    }
 
-    template <class _Key, class _T, class _Compare, class _Allocator>
-    bool operator==(const map<_Key,_T,_Compare,_Allocator>& lhs, const map<_Key,_T,_Compare,_Allocator>& rhs)
-    {
+    template <class Key, class T, class Compare, class Allocator>
+    bool operator==(const map<Key,T,Compare,Allocator>& lhs, const map<Key,T,Compare,Allocator>& rhs){
         if (lhs.size() != rhs.size())
             return false;
         return ft::equal(lhs.begin(), lhs.end(), rhs.begin());
--- include/set.hpp
+++ include/set.hpp
@@ -15,14 +15,14 @@ namespace ft {
         typedef typename Ax::template rebind<value_type>::other     
                     allocator_type;
         typedef key_compare     value_compare;
-        enum {Multi = Mfl};
-        Tset_traits() : comp() {}
-        Tset_traits(Pr Parg) : comp(Parg) {}
         struct Kfn {
             const K& operator()(const value_type& X) const {
                 return (X);
             };
         };
+        enum {Multi = Mfl};
+        Tset_traits() : comp() {}
+        Tset_traits(Pr Parg) : comp(Parg) {}
         Pr  comp;
     };
 
@@ -33,21 +33,21 @@ namespace ft {
     public:
         typedef set<K, Pr, A>   Myt;
         typedef ft::Tree<Tset_traits<K, Pr, A, false> >     Mybase;
-        typedef K   key_type;
-        typedef Pr  key_compare;
-        typedef typename Mybase::value_compare  value_compare;
-        typedef typename Mybase::allocator_type allocator_type;
-        typedef typename Mybase::size_type       size_type;
-        typedef typename Mybase::difference_type    difference_type;
-        typedef typename Mybase::pointer        pointer;
-        typedef typename Mybase::const_pointer  const_pointer;
-        typedef typename Mybase::reference      reference;
-        typedef typename Mybase::const_reference    const_reference;
-        typedef typename Mybase::iterator       iterator;
-        typedef typename Mybase::const_iterator const_iterator;
-        typedef typename Mybase::reverse_iterator   reverse_iterator;
-        typedef typename Mybase::const_reverse_iterator const_reverse_iterator;
-        typedef typename Mybase::value_type     value_type;
+        typedef K                                           key_type;
+        typedef Pr                                          key_compare;
+        typedef typename Mybase::value_compare              value_compare;
+        typedef typename Mybase::allocator_type             allocator_type;
+        typedef typename Mybase::size_type                  size_type;
+        typedef typename Mybase::difference_type            difference_type;
+        typedef typename Mybase::pointer                    pointer;
+        typedef typename Mybase::const_pointer              const_pointer;
+        typedef typename Mybase::reference                  reference;
+        typedef typename Mybase::const_reference            const_reference;
+        typedef typename Mybase::iterator                   iterator;
+        typedef typename Mybase::const_iterator             const_iterator;
+        typedef typename Mybase::reverse_iterator           reverse_iterator;
+        typedef typename Mybase::const_reverse_iterator     const_reverse_iterator;
+        typedef typename Mybase::value_type                 value_type;
 
         set() : Mybase(key_compare(), allocator_type()) {}
         explicit set(const key_compare& Pred) : Mybase(Pred, allocator_type()) {}
@@ -68,8 +68,21 @@ namespace ft {
             for (; F != L; ++F)
                 this->insert (*F);
         }
-
     };
+
+    template <class Key, class Compare, class Allocator>
+    void swap(set<Key,Compare,Allocator>& lhs, set<Key,Compare,Allocator>& rhs){
+        lhs.swap(rhs);
+    }
+
+    // template <class Key, class Compare, class Allocator>
+    // bool operator==(const set<Key,Compare,Allocator>& lhs, const set<Key,Compare,Allocator>& rhs)
+    // {
+    //     if (lhs.size() != rhs.size())
+    //         return false;
+    //     return ft::equal(lhs.begin(), lhs.end(), rhs.begin());
+    // }
+    
 }
 
 #endif
